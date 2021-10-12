ba47855a7a2da2d4bd033a5828adb03d9d440041
ArtemKaalik <artjom.kaalik@ivkhk.ee>
2021-10-12 16:18:21 +0300

    Zadanie4 ArtemKaalik

---
+++ src/sptv20zadanie4/SPTV20Zadanie4.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20zadanie4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author ArTIK
+ */
+public class SPTV20Zadanie4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int[] usernum = new int[100];
+    int maximum=100,minimum=1;
+    int num=1;
+    for (int i = 0; i < 100; i++){
+        Random random = new Random();
+
+        if(maximum % 2 == 1)
+        maximum = maximum + 1;
+
+    int range = (maximum - minimum + 1) / 2;
+    usernum[i] = random.nextInt(range) * 2 + minimum;
+
+}
+    int x=0;
+    Arrays.sort(usernum);
+
+    for (int i = 0; i < usernum.length; i++) {
+        System.out.printf("%4d",usernum[i]);
+        x++;
+        if (x==10){
+            System.out.println();
+            x=0;
+        }
+
+    }
+    int summa = 0;
+    for (int i = 0; i < usernum.length; i++) {
+    summa = summa + usernum[i];
+    }
+    int numto=summa/10;
+    System.out.println("Среднее арифметическое:"+numto);
+    }
+    
+}
